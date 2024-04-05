f2513a6828308c899dfb1c5bb5bc78c5bc5a7625
Sam Reginald Besa <besasamreginald36@gmail.com>
2024-04-05 22:38:35 +0800

    - receieved adjustment log table

--- js/rmTables.js
+++ js/rmTables.js
@@ -25,7 +25,7 @@ $(function(){
                     </button>
                 </div>
             </div>
-            <div class="rm-manage-md-card-table-body">
+            <div class="table-body">
                 <table class="table table-striped table-responsive table-hover" id="receiveTable">
                     <thead>
                         <tr>
@@ -48,52 +48,6 @@ $(function(){
                 <div class="pagination-container">
                     <ul class="pagination" id="receivedTablePagination">
                         <!--- rmFetchReceived.js --->
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        <div class="rm-manage-md-card">
-            <div class="rm-manage-md-card-row">
-                <div class="rm-manage-md-card-item">
-                    <h4>Adjustment Log</h4>
-                </div>
-                <div class="rm-manage-md-card-btn-searchbar">
-                    <select id="recAdjSearchFilter" class="form-select form-select-sm dropdown" required>
-                        <option selected hidden value="">Filter by...</option>
-                        <option value="date">Date Adjusted</option>
-                        <option value="time">Time Adjusted</option>
-                        <option value="name">Raw Material</option>
-                        <option value="id">Id</option>
-                        <option value="receiveQuantity">Receive Quantity</option>
-                        <option value="adjustedQuantity">Adjusted Quantity</option>
-                        <option value="user">Adjusted By</option>
-                    </select>
-                    <input id="recAdjSearch" placeholder="Search..." class="form-control form-control-sm" type="text">
-                </div>
-            </div>
-            <div class="rm-manage-md-card-table-body">
-                <table class="table table-striped table-responsive table-hover" id="receiveAdjTable">
-                    <thead>
-                        <tr>
-                            <th scope="col">Date Adjusted</th>
-                            <th scope="col">Time Adjusted</th>
-                            <th scope="col">Raw Material</th>
-                            <th scope="col">Id</th>
-                            <th scope="col">Receive Quantity</th>
-                            <th scope="col">Adjusted Quantity</th>
-                            <th scope="col">Adjusted By</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!--- rmFetchReceivedAdj.js -->
-                    </tbody>
-                </table>
-            </div>
-            <div class="table-footer">
-                <div class="pagination-container">
-                    <ul class="pagination" id="receivedAdjTablePagination">
-                        <!--- rmFetchReceivedAdj.js -->
                     </ul>
                 </div>
             </div>
