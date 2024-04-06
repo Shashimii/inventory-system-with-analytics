04d7efe613358b419b8635b1879110a39e2885c1
Sam Reginald Besa <besasamreginald36@gmail.com>
2024-04-06 11:48:42 +0800

    + In Production Table Data

---
+++ php/rm_InProduction_data.php
@@ -0,0 +1,21 @@
+<?php 
+
+include 'script_con.php';
+
+$stmt = $con->prepare("SELECT action_date, item_name, item_desc, item_id, item_lot, item_bin, quantity_inProduction FROM rm_data WHERE item_data_status = '$dataStatusInProduction' AND item_data_active = '$dataActive' AND quantity_inProduction IS NOT NULL");
+$stmt->execute();
+$result = $stmt->get_result();
+
+$data = array();
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+// Close the connection
+$stmt->close();
+$con->close();
+
+// Send the data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
