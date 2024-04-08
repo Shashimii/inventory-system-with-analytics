c70fc410a19466619f095f3a4d52c94940a3c6f0
Shashimii <besasamreginald36@gmail.com>
2024-04-08 15:26:22 +0800

    + prevent 0 KG usage of raw mat

--- js/rmMarkDepleted.js
+++ js/rmMarkDepleted.js
@@ -13,30 +13,42 @@ $(function() {
             data: inputData,
             success: function(response) {
                 console.log(response)
-                if (response === '0') {
-                    Swal.fire({
-                        title: 'In Production',
-                        text: rm_name + '-' + rm_desc + '-' + rm_id + ' is Marked as Depleted',
-                        icon: 'success',
-                    }).then(function() {
-                        location.reload();
-                    })
-                } else if (response === '1') {
-                    Swal.fire({
-                        title: 'Oops',
-                        text: rm_name + '-' + rm_desc + '-' + rm_id + ' is Already Marked as Depleted',
-                        icon: 'error',
-                    }).then(function() {
-                        location.reload();
-                    })
-                } else if (response === '9') {
-                    Swal.fire({
-                        title: 'Oops',
-                        text: rm_name + '-' + rm_desc + '-' + rm_id + ' Scrap is Greater than Raw Material Quantity',
-                        icon: 'error',
-                    })
-                } else {
-                    console.log('Hello? Something Went Wrong on Submitting this data')
+                switch(response) {
+                    case '0':
+                        Swal.fire({
+                            title: 'In Production',
+                            text: rm_name + '-' + rm_desc + '-' + rm_id + ' is Marked as Depleted',
+                            icon: 'success',
+                        }).then(function() {
+                            location.reload();
+                        })
+                        break;
+                    case '1':
+                        Swal.fire({
+                            title: 'Oops',
+                            text: rm_name + '-' + rm_desc + '-' + rm_id + ' is Already Marked as Depleted',
+                            icon: 'error',
+                        }).then(function() {
+                            location.reload();
+                        })
+                        break;
+                    case '9':
+                        Swal.fire({
+                            title: 'Oops',
+                            text: 'Scrap is Greater than Raw Material Quantity',
+                            icon: 'error',
+                        })
+                        break;
+                    case '8':
+                        Swal.fire({
+                            title: 'Oops',
+                            text: 'Raw Material is not Used',
+                            icon: 'error',
+                        })
+                        break;
+                    default:
+                        console.log('Hello? Something Went Wrong on Submitting this data')
+                        break;
                 }
             },
             error: function(xhr, status, error) {
