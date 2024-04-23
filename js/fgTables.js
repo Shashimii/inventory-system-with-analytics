b05647bf7b7f4937c74da5699673615ea650f55b
Shashimii <besasamreginald36@gmail.com>
2024-04-23 21:23:42 +0800

    + fg history table

--- js/fgTables.js
+++ js/fgTables.js
@@ -54,4 +54,58 @@ $(function(){
         $('#renderfgTable').append(tableStructure);
     }
     fgRecTableStructure();
+
+
+    function fgHistoryTableStructure() {
+        $('#renderHistoryTable').empty(); 
+        var tableStructure = `
+        <div class="rm-manage-md-card">
+            <div class="rm-manage-md-card-row">
+                <div class="rm-manage-md-card-item">
+                    <h4>Transaction History</h4>
+                </div>
+                <div class="rm-manage-md-card-btn-searchbar">
+                <select id="searchFilter" class="form-select form-select-sm dropdown" required>
+                    <option selected value="">Id Lot Bin</option>
+                    <option value="date">Date Created</option>
+                    <option value="time">Time Created</option>
+                    <option value="name">FG Name</option>
+                    <option value="desc">FG Description</option>
+                </select>
+                    <input id="searchBar" placeholder="Search..." class="form-control form-control-sm" type="text">
+                </div>
+            </div>
+            <div class="table-body">
+                <table class="table table-striped table-responsive table-hover" id="historyTable">
+                    <thead>
+                        <tr>
+                            <th scope="col">Date Created</th>
+                            <th scope="col">Time Created</th>
+                            <th scope="col">Raw Material Name</th>
+                            <th scope="col">Raw Material Id</th>
+                            <th scope="col">Finished Goods</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Lot</th>
+                            <th scope="col">Bin</th>
+                            <th scope="col" class="action-btn">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="table-footer">
+                <div class="pagination-container">
+                    <ul class="pagination" id="historyTablePagination">
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `;
+
+        $('#renderHistoryTable').append(tableStructure);
+    }
+    fgHistoryTableStructure();
 });
