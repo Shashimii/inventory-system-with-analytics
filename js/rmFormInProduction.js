c8bab05dc1b1786f1e2473da117c6faa69310b7e
Shashimii <besasamreginald36@gmail.com>
2024-04-26 18:53:40 +0800

    # in production alert visual improvements

--- js/rmFormInProduction.js
+++ js/rmFormInProduction.js
@@ -4,7 +4,9 @@ $(function() { // document ready function
         
         var rm_name = $('#itemName').val(); 
         var rm_desc = $('#itemDesc').val(); 
-        var rm_id = $('#itemId').val(); 
+        var rm_id = $('#itemId').val();
+        var rm_lot = $('#itemLot').val();
+        var rm_bin = $('#itemBin').val(); 
         var rm_quantity = $('#quantityReceive').val(); 
         var inputData = $(this).serialize();
 
@@ -16,8 +18,8 @@ $(function() { // document ready function
                 console.log(response)
                 if (response === '0') {
                     Swal.fire({
-                        title: 'In Production',
-                        text: rm_name + '-' + rm_desc + '-' + rm_id + '-' + rm_quantity + ' KG is In Production Now',
+                        title: 'Raw Material In Production',
+                        html: '<div style="text-align: center;"><p>Raw Material Info</p><table style="margin: 0 auto; text-align: left;"><tr><td><b>Name:</b></td><td>' + rm_name + '</td></tr><tr><td><b>Description:</b></td><td>' + rm_desc + '</td></tr><tr><td><b>Id:</b></td><td>' + rm_id + '</td></tr><tr><td><b>Lot:</b></td><td>' + rm_lot + '</td></tr><tr><td><b>Bin:</b></td><td>' + rm_bin + '</td></tr><tr><td><b>Quantity (kg):</b></td><td>' + rm_quantity + ' kg</td></tr></table></div>',
                         icon: 'success',
                     }).then(function() {
                         location.reload();
@@ -25,7 +27,7 @@ $(function() { // document ready function
                 } else if (response === '1') {
                     Swal.fire({
                         title: 'Oops',
-                        text: rm_name + '-' + rm_desc + '-' + rm_id + '-' + rm_quantity + ' KG is Already In Production',
+                        text: 'Already In Production',
                         icon: 'error',
                     }).then(function() {
                         location.reload();
