c7b9903bfac944d690878506a226bdea7ca1f98f
Shashimii <besasamreginald36@gmail.com>
2024-05-06 08:22:45 +0800

    # fix computation query

--- php/rm_status_data.php
+++ php/rm_status_data.php
@@ -2,13 +2,12 @@
 include 'script_con.php';
 
 $stmt = $con->prepare("SELECT item_desc,
-    SUM(quantity_receive) AS total_quantity,
-    SUM(quantity_inProduction) AS total_inProduction,
-    SUM(quantity_scrap) AS total_scrap,
+    SUM(CASE WHEN (item_data_active = 'Y') THEN quantity_receive ELSE 0 END) AS total_quantity,
+    SUM(CASE WHEN (item_data_active = 'Y') THEN quantity_inProduction ELSE 0 END) AS total_inProduction,
+    SUM(CASE WHEN (item_data_active = 'Y') THEN quantity_scrap ELSE 0 END) AS total_scrap,
     SUM(CASE WHEN (item_data_active = 'Y' OR item_data_active = 'N') THEN quantity_used ELSE 0 END) AS total_used,
-    SUM(quantity_created_pcs) AS total_pcs
-    FROM `rm_data`
-    WHERE item_data_active = 'Y'
+    SUM(CASE WHEN (item_data_active = 'Y') THEN quantity_created_pcs ELSE 0 END) AS total_pcs
+    FROM rm_data
     GROUP BY item_desc
     ORDER BY id DESC");
 $stmt->execute();
